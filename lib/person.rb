fc9bede52cb9a35de536e215dfdbc87b43eece20
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-27 19:15:13 -0400

    complete assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+  # Use keyword arguments to define an initialize method
+  # Use mass assignment to metaprogram an initialize method
+
+  def initialize attributes
+    attributes.each do |key, value|
+      self.class.attr_accessor key
+      send "#{key}=", value
+    end
+  end
 end
